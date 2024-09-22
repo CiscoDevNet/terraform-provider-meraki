9576460e0269b45b781f2daa7d7dd98f9ee75736
danischm <danischm@cisco.com>
2024-09-22 11:42:21 +0200

    Add wireless network electronic shelf label resource and data source

---
+++ examples/resources/meraki_wireless_network_electronic_shelf_label/import.sh
@@ -0,0 +1,1 @@
+terraform import meraki_wireless_network_electronic_shelf_label.example "<network_id>"
---
+++ examples/resources/meraki_wireless_network_electronic_shelf_label/resource.tf
@@ -0,0 +1,5 @@
+resource "meraki_wireless_network_electronic_shelf_label" "example" {
+  network_id = "L_123456"
+  enabled    = true
+  hostname   = "N_24329156"
+}
