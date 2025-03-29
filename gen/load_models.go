6c8fc7b4fc2c4a2d4a82c9284cae0593139025fb
danischm <danischm@cisco.com>
2025-03-29 10:22:17 +0100

    Bump pre prelease model

--- gen/load_models.go
+++ gen/load_models.go
@@ -30,7 +30,7 @@ import (
 
 var models = []string{
 	"https://raw.githubusercontent.com/meraki/openapi/refs/tags/v1.56.0/openapi/spec3.json",
-	"https://raw.githubusercontent.com/meraki/openapi/refs/tags/v1.56.0-beta.0/openapi/spec3.json",
+	"https://raw.githubusercontent.com/meraki/openapi/refs/tags/v1.56.0-beta.3/openapi/spec3.json",
 }
 
 const (
