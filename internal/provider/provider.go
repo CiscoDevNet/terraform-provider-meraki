4ec6bc43755684a92f9a6b3721259eee36334b7d
GitHub Action <action@github.com>
2025-10-01 18:06:33 +0000

    Update UserAgent to version 1.8.0

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -280,6 +280,7 @@ func (p *MerakiProvider) Configure(ctx context.Context, req provider.ConfigureRe
 		return
 	}
 
+	c.UserAgent = "MerakiTerraform/1.8.0 Cisco"
 	data := MerakiProviderData{Client: &c}
 	resp.DataSourceData = &data
 	resp.ResourceData = &data
