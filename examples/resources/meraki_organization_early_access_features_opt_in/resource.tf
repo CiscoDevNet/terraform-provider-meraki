5aa7587779cbbed39b94d7e25e8eb7437cdcd9c6
danischm <danischm@cisco.com>
2026-03-14 10:57:38 +0100

    Fix early acess test data

--- examples/resources/meraki_organization_early_access_features_opt_in/resource.tf
+++ examples/resources/meraki_organization_early_access_features_opt_in/resource.tf
@@ -1,5 +1,5 @@
 resource "meraki_organization_early_access_features_opt_in" "example" {
   organization_id         = "123456"
-  short_name              = "has_mx_no_nat_early_access"
+  short_name              = "has_vlan_db"
   limit_scope_to_networks = ["N_12345"]
 }
