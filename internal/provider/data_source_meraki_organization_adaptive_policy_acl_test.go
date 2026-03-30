ddf3f11c997f1226f557efa0f9aa9a6b8f9d8689
danischm <danischm@cisco.com>
2026-03-30 17:34:50 +0200

    Fix adaptive policy acl test data

--- internal/provider/data_source_meraki_organization_adaptive_policy_acl_test.go
+++ internal/provider/data_source_meraki_organization_adaptive_policy_acl_test.go
@@ -39,7 +39,7 @@ func TestAccDataSourceMerakiOrganizationAdaptivePolicyACL(t *testing.T) {
 	checks = append(checks, resource.TestCheckResourceAttr("data.meraki_organization_adaptive_policy_acl.test", "name", "Block sensitive web traffic"))
 	checks = append(checks, resource.TestCheckResourceAttr("data.meraki_organization_adaptive_policy_acl.test", "rules.0.dst_port", "22-30"))
 	checks = append(checks, resource.TestCheckResourceAttr("data.meraki_organization_adaptive_policy_acl.test", "rules.0.log", "true"))
-	checks = append(checks, resource.TestCheckResourceAttr("data.meraki_organization_adaptive_policy_acl.test", "rules.0.policy", "deny"))
+	checks = append(checks, resource.TestCheckResourceAttr("data.meraki_organization_adaptive_policy_acl.test", "rules.0.policy", "allow"))
 	checks = append(checks, resource.TestCheckResourceAttr("data.meraki_organization_adaptive_policy_acl.test", "rules.0.protocol", "tcp"))
 	checks = append(checks, resource.TestCheckResourceAttr("data.meraki_organization_adaptive_policy_acl.test", "rules.0.src_port", "1,33"))
 	checks = append(checks, resource.TestCheckResourceAttr("data.meraki_organization_adaptive_policy_acl.test", "rules.0.tcp_established", "true"))
@@ -80,7 +80,7 @@ func testAccDataSourceMerakiOrganizationAdaptivePolicyACLConfig() string {
 	config += `  rules = [{` + "\n"
 	config += `    dst_port = "22-30"` + "\n"
 	config += `    log = true` + "\n"
-	config += `    policy = "deny"` + "\n"
+	config += `    policy = "allow"` + "\n"
 	config += `    protocol = "tcp"` + "\n"
 	config += `    src_port = "1,33"` + "\n"
 	config += `    tcp_established = true` + "\n"
@@ -106,7 +106,7 @@ func testAccNamedDataSourceMerakiOrganizationAdaptivePolicyACLConfig() string {
 	config += `  rules = [{` + "\n"
 	config += `    dst_port = "22-30"` + "\n"
 	config += `    log = true` + "\n"
-	config += `    policy = "deny"` + "\n"
+	config += `    policy = "allow"` + "\n"
 	config += `    protocol = "tcp"` + "\n"
 	config += `    src_port = "1,33"` + "\n"
 	config += `    tcp_established = true` + "\n"
