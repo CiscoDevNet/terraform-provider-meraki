b3650fac7f9b1f10281e0300ce8fc57838d99135
Witaut Bajaryn <wbajaryn@cisco.com>
2026-06-30 10:44:12 +0200

    network_firmware_upgrades: Ensure upgradeWindow is lowercase on state refresh
    The API converts the lowercase enum to capitalized
    ("Mon" instead of "mon")
    despite it being lowercase in the OpenAPI spec,
    causing idempotency issues.
    
    Convert it back to lowercase on state refresh.

--- internal/provider/model_meraki_network_firmware_upgrades.go
+++ internal/provider/model_meraki_network_firmware_upgrades.go
@@ -22,6 +22,7 @@ import (
 	"context"
 	"fmt"
 	"net/url"
+	"strings"
 
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/netascode/go-meraki"
@@ -335,7 +336,9 @@ func (data *NetworkFirmwareUpgrades) fromBody(ctx context.Context, res meraki.Re
 		data.ProductsWirelessControllerParticipateInNextBetaRelease = types.BoolNull()
 	}
 	if value := res.Get("upgradeWindow.dayOfWeek"); value.Exists() && value.Value() != nil {
-		data.UpgradeWindowDayOfWeek = types.StringValue(value.String())
+		// The API converts the lowercase enum to capitalized ("Mon" instead of "mon")
+		// despite it being lowercase in the OpenAPI spec.
+		data.UpgradeWindowDayOfWeek = types.StringValue(strings.ToLower(value.String()))
 	} else {
 		data.UpgradeWindowDayOfWeek = types.StringNull()
 	}
@@ -345,10 +348,6 @@ func (data *NetworkFirmwareUpgrades) fromBody(ctx context.Context, res meraki.Re
 		data.UpgradeWindowHourOfDay = types.StringNull()
 	}
 }
-
-// End of section. //template:end fromBody
-
-// Section below is generated&owned by "gen/generator.go". //template:begin fromBodyPartial
 
 // fromBodyPartial reads values from a gjson.Result into a tfstate model. It ignores null attributes in order to
 // uncouple the provider from the exact values that the backend API might summon to replace nulls. (Such behavior might
@@ -406,7 +405,9 @@ func (data *NetworkFirmwareUpgrades) fromBodyPartial(ctx context.Context, res me
 		data.ProductsWirelessControllerParticipateInNextBetaRelease = types.BoolNull()
 	}
 	if value := res.Get("upgradeWindow.dayOfWeek"); value.Exists() && !data.UpgradeWindowDayOfWeek.IsNull() {
-		data.UpgradeWindowDayOfWeek = types.StringValue(value.String())
+		// The API converts the lowercase enum to capitalized ("Mon" instead of "mon")
+		// despite it being lowercase in the OpenAPI spec.
+		data.UpgradeWindowDayOfWeek = types.StringValue(strings.ToLower(value.String()))
 	} else {
 		data.UpgradeWindowDayOfWeek = types.StringNull()
 	}
@@ -416,8 +417,6 @@ func (data *NetworkFirmwareUpgrades) fromBodyPartial(ctx context.Context, res me
 		data.UpgradeWindowHourOfDay = types.StringNull()
 	}
 }
-
-// End of section. //template:end fromBodyPartial
 
 // Section below is generated&owned by "gen/generator.go". //template:begin fromBodyUnknowns
 
